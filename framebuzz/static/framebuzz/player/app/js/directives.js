0ff6eb4024a38b302212e2a44724bb03e16c9e0d
Dani Roxberry <dani@bitoriented.com>
2013-07-09 13:36:14 -0500

    Use proper Angular.js directives when loading the video player!

--- framebuzz/static/framebuzz/player/app/js/directives.js
+++ framebuzz/static/framebuzz/player/app/js/directives.js
@@ -3,9 +3,40 @@
 /* Directives */
 
 
-angular.module('framebuzz.directives', []).
-  directive('appVersion', ['version', function(version) {
+angular.module('framebuzz.directives', [])
+  .directive('appVersion', 'version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  })
+  .directive('mediaElement', function() {
+    return function(scope, element, attrs) {
+        $(element).mediaelementplayer({
+            features: ['youtube','progress'],
+            flashName: '../../swf/flashmediaelement.swf',
+            silverlightName: '../../swf/silverlightmediaelement.xap',
+            alwaysShowControls: true,
+            // There's a bug here where commenting and hitting the spacebar will
+            // cause the space to not be entered, and the video to pause.
+            enableKeyboard: false,
+            timerRate: 900,
+            success: function (media) {
+                videoPlayer = media;
+
+                media.addEventListener('timeupdate', function(e) {
+                    var currentTimeHMS = convertSecondsToHMS(media.currentTime);
+                    $('span.add-on em.current').text(currentTimeHMS);
+                }, false);
+
+                media.addEventListener('playing', function(e) {
+
+                }, false);
+
+                media.addEventListener('pause', function(e) {
+                    $('.mejs-video').unbind('hover');
+                    $('.mejs-overlay-play').hide().addClass('mejs-overlay-pause').fadeIn(500).show();
+                }, false);
+            }
+        });
+    };
+});
--- framebuzz/static/framebuzz/player/app/js/ui.js
+++ framebuzz/static/framebuzz/player/app/js/ui.js
@@ -13,34 +13,3 @@ var convertSecondsToHMS = function(totalSec) {
       + (seconds  < 10 ? "0" + seconds : seconds);
   }
 };
-
-
-$(document).ready(function() {
-  var player = new MediaElementPlayer('#ytVideo', {
-    features: ['youtube','progress'],
-    flashName: '../../swf/flashmediaelement.swf',
-    silverlightName: '../../swf/silverlightmediaelement.xap',
-    alwaysShowControls: true,
-    // There's a bug here where commenting and hitting the spacebar will
-    // cause the space to not be entered, and the video to pause.
-    enableKeyboard: false,
-    timerRate: 900,
-    success: function (media) {
-      videoPlayer = media;
-
-      media.addEventListener('timeupdate', function(e) {
-        var currentTimeHMS = convertSecondsToHMS(media.currentTime);
-        $('span.add-on em.current').text(currentTimeHMS);
-      }, false);
-
-      media.addEventListener('playing', function(e) {
-
-      }, false);
-
-      media.addEventListener('pause', function(e) {
-        $('.mejs-video').unbind('hover');
-        $('.mejs-overlay-play').hide().addClass('mejs-overlay-pause').fadeIn(500).show();
-      }, false);
-    }
-  });
-});
