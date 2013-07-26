7ffb7e56624105f3550278fb2f6351ee36f9e219
Dani Roxberry <dani@bitoriented.com>
2013-07-25 21:29:24 -0500

    I can add Sentry to Angular? I'm in love.

--- framebuzz/static/framebuzz/player/app/js/app.js
+++ framebuzz/static/framebuzz/player/app/js/app.js
@@ -4,6 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('framebuzz', 
     [
+        'fbExceptions',
         'ngCookies',
         'ngSanitize',
         'ui.state', 
