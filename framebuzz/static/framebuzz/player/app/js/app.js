1f3a2519ec3408015c84cab8f09d38492b80c126
Dani Roxberry <dani@bitoriented.com>
2013-07-31 02:31:33 -0500

    Title truncation if required @done

--- framebuzz/static/framebuzz/player/app/js/app.js
+++ framebuzz/static/framebuzz/player/app/js/app.js
@@ -4,6 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('framebuzz', 
     [
+        'truncate',
         'fbExceptions',
         'ngCookies',
         'ngSanitize',
