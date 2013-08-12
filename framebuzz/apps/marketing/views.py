8f4eecb9d8f1ff0da0141a4a442f01eee207df77
Dani Roxberry <dani@bitoriented.com>
2013-08-12 17:58:35 -0500

    * social icons share the page - make like normal ones @done * Fix background on active links in header: profile/homepage. @done

--- framebuzz/apps/marketing/views.py
+++ framebuzz/apps/marketing/views.py
@@ -1,3 +1,8 @@
+import urllib2
+import lxml.html as lh
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
@@ -41,3 +46,12 @@ def press(request):
     {
     },
     context_instance=RequestContext(request))
+
+
+def google_plus_count(request):
+    doc = lh.parse(urllib2.urlopen('https://plusone.google.com/_/+1/fastbutton?url=http://framebuzz.com&count=true'))
+    count = doc.xpath("//div[@id='aggregateCount']/text()")
+    print str(count)
+
+    response = { 'count': str(count).lstrip("['").rstrip("']") }
+    return HttpResponse(json.dumps(response), content_type='application/json')
