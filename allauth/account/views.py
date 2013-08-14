14722deaf3470945bffbaa991adf9c83d9b15c0a
Dani Roxberry <dani@bitoriented.com>
2013-08-14 01:29:08 -0500

    Fabulous login page.

--- allauth/account/views.py
+++ allauth/account/views.py
@@ -68,18 +68,15 @@ class LoginView(RedirectAuthenticatedUserMixin, FormView):
 
     def get_context_data(self, **kwargs):
         ret = super(LoginView, self).get_context_data(**kwargs)
-        nextUrl = self.request.GET.get('nextUrl', None)
-        site = Site.objects.get_current()
-        ret.update({
-                "signup_url": passthrough_next_redirect_url(self.request,
-                                                            reverse("account_signup"),
-                                                            self.redirect_field_name),
-                "site": site,
-                "redirect_field_name": self.redirect_field_name,
-                "redirect_field_value": self.request.REQUEST.get(self.redirect_field_name),
-                "is_ajax_request": self.request.is_ajax(),
-                "next_url": None if nextUrl is None else nextUrl.replace(site.domain, '').replace('http://', '').replace('www.', '')
-                })
+        signup_url = passthrough_next_redirect_url(self.request,
+                                                   reverse("account_signup"),
+                                                   self.redirect_field_name)
+        redirect_field_value = self.request.REQUEST \
+            .get(self.redirect_field_name)
+        ret.update({"signup_url": signup_url,
+                    "site": Site.objects.get_current(),
+                    "redirect_field_name": self.redirect_field_name,
+                    "redirect_field_value": redirect_field_value})
         return ret
 
 login = LoginView.as_view()
