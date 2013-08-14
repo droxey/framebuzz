7ed5ec4709e4402c89d83120fe95ab2b481d8ac6
Dani Roxberry <dani@bitoriented.com>
2013-08-14 12:09:57 -0500

    Fix a bug that would cause profiles to break if there was a '.' in the username.

--- framebuzz/apps/profiles/urls.py
+++ framebuzz/apps/profiles/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^(?P<username>\w+)/$', 'framebuzz.apps.profiles.views.home', name='profiles-home'),
-    url(r'^(?P<username>\w+)/activity/$', 'framebuzz.apps.profiles.views.activity', name='profiles-activity'),
-    url(r'^(?P<username>\w+)/followers/$', 'framebuzz.apps.profiles.views.profile_followers', name='profiles-followers'),
-    url(r'^(?P<username>\w+)/following/$', 'framebuzz.apps.profiles.views.profile_following', name='profiles-following'),
-    url(r'^(?P<username>\w+)/favorites/$', 'framebuzz.apps.profiles.views.favorites', name='profiles-favorites'),
-    url(r'^(?P<username>\w+)/conversations/$', 'framebuzz.apps.profiles.views.conversations', name='profiles-conversations'),
-    url(r'^(?P<username>\w+)/videos/$', 'framebuzz.apps.profiles.views.videos', name='profiles-videos'),
+    url(r'^(?P<username>[\w.@+-]+)/$', 'framebuzz.apps.profiles.views.home', name='profiles-home'),
+    url(r'^(?P<username>[\w.@+-]+)/activity/$', 'framebuzz.apps.profiles.views.activity', name='profiles-activity'),
+    url(r'^(?P<username>[\w.@+-]+)/followers/$', 'framebuzz.apps.profiles.views.profile_followers', name='profiles-followers'),
+    url(r'^(?P<username>[\w.@+-]+)/following/$', 'framebuzz.apps.profiles.views.profile_following', name='profiles-following'),
+    url(r'^(?P<username>[\w.@+-]+)/favorites/$', 'framebuzz.apps.profiles.views.favorites', name='profiles-favorites'),
+    url(r'^(?P<username>[\w.@+-]+)/conversations/$', 'framebuzz.apps.profiles.views.conversations', name='profiles-conversations'),
+    url(r'^(?P<username>[\w.@+-]+)/videos/$', 'framebuzz.apps.profiles.views.videos', name='profiles-videos'),
 )
