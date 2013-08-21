5f4383dcf069836e620376c46f5e73daa1eaaa36
Dani Roxberry <dani@bitoriented.com>
2013-08-21 18:04:27 -0500

    Fix FF heatmap progress bar bug. Refs #136.

--- framebuzz/static/framebuzz/player/app/js/directives.js
+++ framebuzz/static/framebuzz/player/app/js/directives.js
@@ -28,14 +28,29 @@ angular.module('framebuzz.directives', [])
                 enablePluginSmoothing: true,
                 autosizeProgress: false,
                 success: function(media) {
+                    var isOpera = !!window.opera || navigator.userAgent.indexOf('Opera') >= 0;
+                    // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
+                    var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
+                    var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+                    // At least Safari 3+: "[object HTMLElementConstructor]"
+                    var isChrome = !!window.chrome;                          // Chrome 1+
+                    var isIE = /*@cc_on!@*/false;                            // At least IE6
+
+
                     $('.mejs-video').css({ height: '385px', width: '640px' });
                     $('video').css({ height: '385px', width: '640px' });
 
                     $rootScope.player = media;
                     safeApply($rootScope);
 
-                    if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
+                    if (isSafari && !isChrome) {
                         $('.mejs-time-rail').addClass('safari');
+                    }
+
+                    if (isFirefox) {
+                        if (navigator.appVersion.indexOf('Windows') != -1) {
+                            $('.mejs-time-rail').addClass('winff');
+                        }
                     }
 
                     $('.mejs-volume-button').addClass('mejs-fade-in');
