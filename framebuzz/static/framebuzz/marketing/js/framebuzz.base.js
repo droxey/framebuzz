2010430f581ba9dd08ae1d5e16067718dd431381
Dani Roxberry <dani@bitoriented.com>
2013-11-03 15:13:08 -0600

    Sexify full screen bkg. Option to make it a slideshow too.

--- framebuzz/static/framebuzz/marketing/js/framebuzz.base.js
+++ framebuzz/static/framebuzz/marketing/js/framebuzz.base.js
@@ -1,4 +1,12 @@
-$(document).ready(function() {
+$(function() {
+    $.vegas({
+        src: '/static/framebuzz/marketing/img/background.jpg'
+    });
+    
+    $.vegas('overlay', {
+        src:'/static/framebuzz/marketing/vendor/jquery/vegas/overlays/04.png'
+    });
+
     $.get('/accounts/login/', function(html) {
         $('#login-container').html(html);
     });
