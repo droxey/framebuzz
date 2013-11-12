98e72653629a78a7d44a12ecf19bbf3ad857188d
Dani Roxberry <dani@bitoriented.com>
2013-11-12 03:10:28 -0600

    Login, password reset step 1, password reset confirm, password reset form.

--- framebuzz/static/framebuzz/marketing/js/framebuzz.base.js
+++ framebuzz/static/framebuzz/marketing/js/framebuzz.base.js
@@ -6,10 +6,4 @@ $(function() {
     $.vegas('overlay', {
         src:'/static/framebuzz/marketing/vendor/jquery/vegas/overlays/04.png'
     });
-
-    if ($('#login-container').length > 0) {
-        $.get('/accounts/login/', function(html) {
-            $('#login-container').html(html);
-        });
-    }
 });
