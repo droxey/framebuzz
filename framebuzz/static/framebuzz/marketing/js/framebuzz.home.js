097abb40cc75516778c71fafa45ce39ba82d7193
Dani Roxberry <dani@bitoriented.com>
2013-12-03 12:55:00 -0600

    Sweet js!

--- framebuzz/static/framebuzz/marketing/js/framebuzz.home.js
+++ framebuzz/static/framebuzz/marketing/js/framebuzz.home.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+    var taglines = [
+        'Story + Community = <strong>Meaningful Conversations</strong>',
+        'Don’t Just Share a Video. <strong>Share the Experience of Watching It</strong>.',
+        'Community-Curated <strong>Online Conversations</strong>',
+        'Taking Social Media to the <strong>Next Level of Conversation</strong>',
+        'Go Beyond Video Comments – Get Into a <strong>Meaningful Conversation</strong>'
+    ];
+
+    var currentTaglineIndex = Math.floor(Math.random() * taglines.length);
+    var taglineSpan = $('#tagline > span');
+    taglineSpan.html(taglines[currentTaglineIndex]);
+    
+    setInterval(function() { 
+        taglineSpan.fadeOut('fast', function() {
+            currentTaglineIndex = (currentTaglineIndex == 4) ? 0 : (currentTaglineIndex + 1); 
+            taglineSpan.html(taglines[currentTaglineIndex]);
+            taglineSpan.fadeIn('fast');
+        });    
+    }, 10000);
+
     $('.slider > ul').bxSlider({
       auto: true,
       controls: false,
