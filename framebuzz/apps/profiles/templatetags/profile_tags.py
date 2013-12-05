7fc7e87ddff8f3fc2d97dfa6fb817d6c13fa3c13
Dani Roxberry <dani@bitoriented.com>
2013-12-05 16:32:24 -0600

    Finally get short numbers working properly in the stats.

--- framebuzz/apps/profiles/templatetags/profile_tags.py
+++ framebuzz/apps/profiles/templatetags/profile_tags.py
@@ -1,3 +1,4 @@
+import math
 from dateutil import tz, parser as date_parser
 
 from django import template
@@ -16,3 +17,20 @@ def display_name(user):
     if not isinstance(user, User):
         return False
     return user.get_profile().display_name or user.username
+
+
+@register.filter
+def short_number(x):
+    if x == 0: return '0'
+    magnitude = int(math.log10(abs(x)))
+    if magnitude < 3:
+        return x
+    if magnitude > 13: 
+        format_str = '%i t'
+        denominator_mag = 12
+    else: 
+        float_fmt = '%2.1f' if magnitude % 3 == 1 else '%1.2f'
+        illion = (magnitude + 1) // 3
+        format_str = float_fmt + ['', 'k', 'm', 'b', 't'][illion]
+        denominator_mag = illion * 3
+    return (format_str % (x * 1.0 / (10 ** denominator_mag))).lstrip('0')
