8f61a7c939440764aeb49623f2fe7155cd04e471
Dani Roxberry <dani@bitoriented.com>
2014-04-07 14:57:58 -0500

    Update JS for new Sentry server.

--- framebuzz/static/framebuzz/player/app/js/exceptions.js
+++ framebuzz/static/framebuzz/player/app/js/exceptions.js
@@ -2,7 +2,9 @@ angular.module('fbExceptions', []).factory('$exceptionHandler',
           ['$window', '$log',
   function ($window,   $log) {
     if ($window.Raven) {
-      Raven.config('https://b14b841441f348b2bd29aa34c79948fb@app.getsentry.com/11190').install();
+        Raven.config('http://dcacaf02c69f45dda45b12a8c2287178@sentry.framebuzzlab.com/2', {
+            whitelistUrls: [/framebuzz\.com/]
+        }).install();
       return function (exception, cause) {
         $log.error.apply($log, arguments);
         Raven.captureException(exception);
