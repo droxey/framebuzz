f7f2f9fc4b59f7065539c770780f417b933352bc
Dani Roxberry <dani@bitoriented.com>
2014-04-16 18:40:51 -0500

    Homepage assets, setup.

--- framebuzz/static/framebuzz/marketing/js/framebuzz.base.js
+++ framebuzz/static/framebuzz/marketing/js/framebuzz.base.js
@@ -1,9 +1,11 @@
 $(function() {
-    $.vegas({
-        src: '/static/framebuzz/marketing/img/background.jpg'
-    });
-    
-    $.vegas('overlay', {
-        src:'/static/framebuzz/marketing/vendor/jquery/vegas/overlays/04.png'
-    });
+    if (!$('body').hasClass('homepage')) {
+        $.vegas({
+            src: '/static/framebuzz/marketing/img/background.jpg'
+        });
+
+        $.vegas('overlay', {
+            src:'/static/framebuzz/marketing/vendor/jquery/vegas/overlays/04.png'
+        });
+    }
 });
--- framebuzz/static/framebuzz/marketing/js/framebuzz.home.js
+++ framebuzz/static/framebuzz/marketing/js/framebuzz.home.js
@@ -1,35 +1,3 @@
 $(document).ready(function() {
-  if ($('body').hasClass('home')) {
-    $('.bxslider').bxSlider({
-      captions: true,
-      pager: false,
-      minSlides: 3,
-      maxSlides: 3,
-      slideMargin: 0,
-      mode: 'vertical',
-      onSliderLoad: function() {
-        $('#thumbs ul li').click(function() {
-          var videoUrl = $(this).find('img').attr('data-video-url'),
-              playerContainer = $('#fbz-player-container'),
-              newFrame = $('<iframe src="" scrolling="no" frameBorder="0" height="445" width="700" class="absolute-center pull-right"></iframe>');
 
-          $('iframe', playerContainer).fadeOut(100, function() {
-            playerContainer.html('');
-            newFrame.attr('src', videoUrl);
-            playerContainer.append(newFrame);
-          });
-        });
-      }
-    });
-  }
-  else {
-    $('.slider > ul').bxSlider({
-      auto: true,
-      controls: false,
-      speed: 500,
-      autoHover: true,
-      pause: 10000,
-      easing: 'easeInOutSine'
-    });
-  }
 });
