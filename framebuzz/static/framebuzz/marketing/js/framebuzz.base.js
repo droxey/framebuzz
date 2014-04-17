59c2859a5e8294e945547e2be4e3d2decf5a9d7e
Dani Roxberry <dani@bitoriented.com>
2014-04-17 16:49:44 -0500

    Additional homepage assets and javascript modifications required to leave the old design intact on all other marketing pages.

--- framebuzz/static/framebuzz/marketing/js/framebuzz.base.js
+++ framebuzz/static/framebuzz/marketing/js/framebuzz.base.js
@@ -1,9 +1,13 @@
 $(function() {
-    if (!$('body').hasClass('homepage')) {
-        $.vegas({
-            src: '/static/framebuzz/marketing/img/background.jpg'
-        });
+    var isHome = $('body').hasClass('homepage');
+    var imgUrl = isHome ? '/static/framebuzz/marketing/img/cover.jpg'
+                        : '/static/framebuzz/marketing/img/background.jpg';
 
+    $.vegas({
+        src: imgUrl
+    });
+
+    if (!isHome) {
         $.vegas('overlay', {
             src:'/static/framebuzz/marketing/vendor/jquery/vegas/overlays/04.png'
         });
