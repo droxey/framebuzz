3fb3c98710335c860d300f7e992fb1bf4d9bc3e5
Dani Roxberry <dani@bitoriented.com>
2014-04-22 06:01:54 -0500

    Nice scrolling action.

--- framebuzz/static/framebuzz/marketing/js/framebuzz.home.js
+++ framebuzz/static/framebuzz/marketing/js/framebuzz.home.js
@@ -3,4 +3,14 @@ $(function() {
     background.backstretch("/static/framebuzz/marketing/img/cover.jpg", {
         centeredY: false,
     });
+
+    $(document).on('click', 'a.anchor', function(e) {
+        var hash = $(this).attr('href');
+        $.scrollTo(hash, 600, {
+            easing:'swing',
+            onAfter: function() {
+            }
+        });
+        return false;
+    });
 });
