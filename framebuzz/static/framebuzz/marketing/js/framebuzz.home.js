0b884ef3a697e99b37e19eaf807d60f3d822c901
Dani Roxberry <dani@bitoriented.com>
2014-04-22 06:42:04 -0500

    Ack only one CSS issue left...

--- framebuzz/static/framebuzz/marketing/js/framebuzz.home.js
+++ framebuzz/static/framebuzz/marketing/js/framebuzz.home.js
@@ -1,7 +1,12 @@
 $(function() {
-    var background = $('#background');
-    background.backstretch("/static/framebuzz/marketing/img/cover.jpg", {
-        centeredY: false,
+    var background = $('#background'),
+        windowHeight = $(window).height();
+
+    background.css({'height': (windowHeight - 50) + 'px'});
+    background.backstretch("/static/framebuzz/marketing/img/cover.jpg", { fade: 10, centeredY: false });
+
+    $(window).on("backstretch.after", function (e, instance, index) {
+        instance.$img.css({'top':''});
     });
 
     $(document).on('click', 'a.anchor', function(e) {
