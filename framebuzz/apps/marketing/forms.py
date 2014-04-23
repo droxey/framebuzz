bf5023b664438ddcaf01706235e93c665f52eaa2
Dani Roxberry <dani@bitoriented.com>
2014-04-23 18:00:24 -0500

    Adam's changes rolled together into a single commit.

--- framebuzz/apps/marketing/forms.py
+++ framebuzz/apps/marketing/forms.py
@@ -3,6 +3,8 @@ from framebuzz.apps.marketing.models import ContactRequest
 
 
 class ContactRequestForm(forms.ModelForm):
+    required_css_class = 'required'
+    
     class Meta:
         model = ContactRequest
         exclude = ('date',)
