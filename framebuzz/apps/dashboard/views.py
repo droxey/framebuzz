7855217a0d22c7ee01797d09924ee85beb692f60
Dani Roxberry <dani@bitoriented.com>
2014-04-26 13:26:56 -0500

    Base page and user sidebar. Added the ability for any user to have a dashboard for simplicity in testing.

--- framebuzz/apps/dashboard/views.py
+++ framebuzz/apps/dashboard/views.py
@@ -1,11 +1,15 @@
 import json
 
+from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
 
+from framebuzz.apps.api.models import UserProfile
 
-def dashboard_home(request):
+
+@login_required
+def dashboard_home(request, username):
     return render_to_response('dashboard/home.html', {
     }, context_instance=RequestContext(request))
