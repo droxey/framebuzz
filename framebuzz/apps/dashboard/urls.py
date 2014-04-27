68f7916b14891eb8110dd362809ab3d6214cacb1
Dani Roxberry <dani@bitoriented.com>
2014-04-27 15:25:48 -0500

    Dashboard login.

--- framebuzz/apps/dashboard/urls.py
+++ framebuzz/apps/dashboard/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     '',
+    url(r'^dashboard/login/$',
+        'framebuzz.apps.dashboard.views.dashboard_login',
+        name='dashboard-login'),
     url(r'^dashboard/(?P<username>[\w.@+-]+)/$',
         'framebuzz.apps.dashboard.views.dashboard_home',
         name='dashboard-home'),
@@ -17,7 +20,4 @@ urlpatterns = patterns(
     url(r'^dashboard/(?P<username>[\w.@+-]+)/settings/$',
         'framebuzz.apps.dashboard.views.dashboard_settings',
         name='dashboard-settings'),
-    url(r'^dashboard/login/$',
-        'framebuzz.apps.dashboard.views.dashboard_login',
-        name='dashboard-login'),
 )
