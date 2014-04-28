c5c5dedcd47a8cae02cd17cf74d359d6c92dc521
Dani Roxberry <dani@bitoriented.com>
2014-04-28 01:34:14 -0500

    Recent audience activity sidebar.

--- framebuzz/apps/dashboard/views.py
+++ framebuzz/apps/dashboard/views.py
@@ -1,17 +1,37 @@
 import json
 
 from django.core.urlresolvers import reverse
+from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
 
+from actstream.models import Action
+
 from framebuzz.apps.api.models import UserProfile, Video, UserVideo
 from framebuzz.apps.dashboard.decorators import require_dashboard
+
+VALID_FEED_VERBS = ['commented on', 'replied to comment',
+                    'added video to library', ]
+
+
+def _get_videos(username):
+    user_videos = UserVideo.objects.filter(user__username=username)
+    videos = [uv.video for uv in user_videos]
+    return videos
 
 
 @require_dashboard
 def dashboard_home(request, username):
+    videos = [v.id for v in _get_videos(username)]
+    activities = Action.objects.filter(Q(
+        Q(action_object_object_id__in=videos) |
+        Q(target_object_id__in=videos)),
+        Q(verb__in=VALID_FEED_VERBS)
+    ).order_by('-timestamp')[:20]
+
     return render_to_response('dashboard/home.html', {
+        'activities': activities,
     }, context_instance=RequestContext(request))
 
 
@@ -23,8 +43,7 @@ def dashboard_profile(request, username):
 
 @require_dashboard
 def dashboard_videos(request, username):
-    user_videos = UserVideo.objects.filter(user=request.user)
-    videos = [uv.video for uv in user_videos]
+    videos = _get_videos(username)
 
     return render_to_response('dashboard/videos.html', {
         'videos': videos,
