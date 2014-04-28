3335c9624d8b161d51b41a1c68a25725061446a3
Dani Roxberry <dani@bitoriented.com>
2014-04-28 08:06:48 -0500

    Silly mistake!

--- framebuzz/apps/dashboard/context_processors.py
+++ framebuzz/apps/dashboard/context_processors.py
@@ -5,14 +5,17 @@ from framebuzz.apps.profiles.forms import AddVideoForm, UploadVideoForm
 
 
 def dashboard(request):
-    form = AddVideoForm(request=request)
-    upload_form = UploadVideoForm(request=request)
-    pending_uploads = Video.objects.exclude(
-        Q(Q(fp_url=None) | Q(job_id=None))).filter(
-            added_by=request.user, processing=True)
+    if request.user.is_authenticated():
+        form = AddVideoForm(request=request)
+        upload_form = UploadVideoForm(request=request)
+        pending_uploads = Video.objects.exclude(
+            Q(Q(fp_url=None) | Q(job_id=None))).filter(
+                added_by=request.user, processing=True)
 
-    return {
-        'form': form,
-        'pending_uploads': pending_uploads,
-        'upload_form': upload_form,
-    }
+        return {
+            'form': form,
+            'pending_uploads': pending_uploads,
+            'upload_form': upload_form,
+        }
+    else:
+        return { }
