4162604ebce582efa3d983f48b9d7aa3bd23d35a
Dani Roxberry <dani@bitoriented.com>
2014-04-28 08:18:06 -0500

    Hotfix 3!

--- framebuzz/apps/dashboard/views.py
+++ framebuzz/apps/dashboard/views.py
@@ -32,29 +32,32 @@ def dashboard_home(request, username):
         Q(verb__in=VALID_FEED_VERBS)
     ).order_by('-timestamp')[:20]
 
-    most_played = Action.objects.filter(
-        verb='viewed video',
-        action_object_object_id__in=videos
-        ).values('action_object_object_id') \
-        .annotate(views=Count('id')) \
-        .order_by('-views')[0]
+    # most_played = Action.objects.filter(
+    #     verb='viewed video',
+    #     action_object_object_id__in=videos
+    #     ).values('action_object_object_id') \
+    #     .annotate(views=Count('id')) \
+    #     .order_by('-views')
 
-    total_views = Action.objects.filter(
-        verb='viewed video',
-        action_object_object_id__in=videos
-    ).aggregate(num_views=Count('id'))
+    # if len(most_played) > 0:
+    #     most_played = most_played[0]
 
-    most_played_video = Video.objects.filter(
-        id=most_played['action_object_object_id'])[0]
-    print most_played_video.default_thumbnail
+    # total_views = Action.objects.filter(
+    #     verb='viewed video',
+    #     action_object_object_id__in=videos
+    # ).aggregate(num_views=Count('id'))
+
+    # most_played_video = Video.objects.filter(
+    #     id=most_played['action_object_object_id'])[0]
+    # print most_played_video.default_thumbnail
 
     return render_to_response('dashboard/home.html', {
         'activities': activities,
-        'most_played': {
-            'total': total_views.get('num_views'),
-            'current': most_played.get('views'),
-            'video': most_played_video,
-        },
+        # 'most_played': {
+        #     'total': total_views.get('num_views'),
+        #     'current': most_played.get('views'),
+        #     'video': most_played_video,
+        # },
         'videos': _get_videos(username),
     }, context_instance=RequestContext(request))
 
