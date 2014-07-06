afc9ed550db5e9e930f9b17422c0d7eb726e13b0
Dani Roxberry <dani@bitoriented.com>
2014-07-05 22:59:48 -0500

    Homepage responsiveness changes.

--- framebuzz/static/framebuzz/marketing/js/framebuzz.home.js
+++ framebuzz/static/framebuzz/marketing/js/framebuzz.home.js
@@ -1,8 +1,10 @@
 $(function() {
     var background = $('#background'),
         windowHeight = $(window).height();
+        nativeHeight = Modernizr.mq('only screen and (max-width: 600px)');
 
-    background.css({'height': windowHeight + 'px'});
+    if(!nativeHeight) { background.css({'height': windowHeight + 'px'}); }
+
     background.backstretch("/static/framebuzz/marketing/img/cover.jpg", { fade: 10, centeredY: false });
 
     $(window).on("backstretch.after", function (e, instance, index) {
@@ -18,4 +20,11 @@ $(function() {
         });
         return false;
     });
+
+    var $stickyHeader = $("#home-floating-search")
+    $(window).on("scroll load", function() {
+        var fromTop = $(document).scrollTop();
+        $stickyHeader.toggleClass("sticky", (fromTop > 25));
+    });
+
 });
