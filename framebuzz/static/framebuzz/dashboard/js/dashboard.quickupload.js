d4826cd687a9061387c8db535f07ede126c07788
Dani Roxberry <dani@bitoriented.com>
2014-09-11 15:43:21 -0500

    menu items, transitions, fix bugs

--- framebuzz/static/framebuzz/dashboard/js/dashboard.quickupload.js
+++ framebuzz/static/framebuzz/dashboard/js/dashboard.quickupload.js
@@ -2,6 +2,45 @@ $(function() {
     // Set filepicker.io key and drop panes for uploads.
     filepicker.setKey('AXQRyfZ2cQjWD3yy2flkFz');
 
+    // Initialize the Quick Upload wizard.
+    $("#wizard").steps({
+        headerTag: "h4",
+        bodyTag: "section",
+        transitionEffect: "slideLeft",
+        autoFocus: true,
+        enableCancelButton: true,
+        enableFinishButton: true,
+        saveState: true,
+        onInit: function (event, currentIndex) {
+            // Add bootstrap styles to the pagination controls.
+            var actionsUl = $('#wizard div.actions ul');
+            var pageButtons = $('li a', actionsUl);
+
+            actionsUl.addClass('clearfix');
+            
+            $.each(pageButtons, function(k, v) {
+                var target = $(v).attr('href'),
+                    hash = target.slice(1),
+                    buttonClass = 'btn btn-small ';
+
+                switch(target) {
+                    case '#cancel':
+                        buttonClass = buttonClass + 'btn-danger';
+                        break;
+                    case '#finish':
+                        buttonClass = buttonClass + 'btn-success';
+                        break;
+                    default:
+                        buttonClass = buttonClass + 'btn-theme';
+                        break;
+                }
+
+                $(v).addClass(buttonClass);
+                $(v).parent().addClass(hash);
+            });
+        }
+    });
+
     $(document).on('click', '#upload-video div.upload-choices div.dmbox', function() {
         var currentChoice = $(this);
         var otherChoices = $('#upload-video div.upload-choices div.dmbox').not(currentChoice);
