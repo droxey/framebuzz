48c48a8bc65c0986c60463ad4c869f50ec4718d1
Dani Roxberry <dani@bitoriented.com>
2014-12-05 12:49:54 -0600

    Link up tasks to original UI

--- todo/settings.py
+++ todo/settings.py
@@ -7,8 +7,7 @@ STAFF_ONLY = getattr(settings, 'TODO_STAFF_ONLY', False)
 first_superuser = User.objects.filter(is_superuser=True)[0]
 DEFAULT_ASSIGNEE = getattr(settings, 'TODO_DEFAULT_ASSIGNEE', first_superuser.username)
 
-first_list = List.objects.first()
-DEFAULT_LIST_ID = getattr(settings, 'TODO_DEFAULT_LIST_ID', first_list.id)
+#first_list = List.objects.first()
+DEFAULT_LIST_ID = 0 #getattr(settings, 'TODO_DEFAULT_LIST_ID', first_list.id)
 
 PUBLIC_SUBMIT_REDIRECT = getattr(settings, 'TODO_PUBLIC_SUBMIT_REDIRECT', '/')
-
