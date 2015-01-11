a3d5775827cb6b347cc80dee60c107099616b582
Dani Roxberry <dani@bitoriented.com>
2015-01-11 15:54:12 -0600

    Remove cruft
    No more django-todo.

--- todo/__init__.py
+++
@@ -1,8 +0,0 @@
-"""django todo"""
-__version__ = '1.5.dev'
-
-__author__ = 'Scot Hacker'
-__email__ = 'shacker@birdhouse.org'
-
-__url__ = 'https://github.com/shacker/django-todo'
-__license__ = 'BSD License'
