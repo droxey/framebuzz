acc1adfcabee9aaad6cc72af7c405bd482ac0292
Dani Roxberry <dani@bitoriented.com>
2015-03-30 07:52:44 -0500

    start merging local > staging

--- framebuzz/static/framebuzz/marketing/js/framebuzz.home.js
+++ framebuzz/static/framebuzz/marketing/js/framebuzz.home.js
@@ -1,16 +1,4 @@
 $(function() {
-    var background = $('#background'),
-        windowHeight = $(window).height();
-        nativeHeight = Modernizr.mq('only screen and (max-width: 600px)');
-
-    if(!nativeHeight) { background.css({'height': windowHeight + 'px'}); }
-
-    background.backstretch("/static/framebuzz/marketing/img/cover.jpg", { fade: 10, centeredY: false });
-
-    $(window).on("backstretch.after", function (e, instance, index) {
-        instance.$img.css({'top':''});
-    });
-
     $(document).on('click', 'a.anchor', function(e) {
         var hash = $(this).attr('href');
         $.scrollTo(hash, 600, {
@@ -21,10 +9,9 @@ $(function() {
         return false;
     });
 
-    var $stickyHeader = $("#home-floating-search")
+    var $stickyHeader = $("#home-floating-search");
     $(window).on("scroll load", function() {
         var fromTop = $(document).scrollTop();
         $stickyHeader.toggleClass("sticky", (fromTop > 25));
     });
-
 });
