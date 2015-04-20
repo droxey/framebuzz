cf827b0e650e47b29646d8e63c19098a9d5c08f5
Dani Roxberry <dani@bitoriented.com>
2015-04-20 17:37:15 -0500

    ahahahah it finally works!!!

--- framebuzz/apps/marketing/forms.py
+++ framebuzz/apps/marketing/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from framebuzz.apps.marketing.models import ContactRequest
+from zebra.forms import StripePaymentForm
 
 
 class ContactRequestForm(forms.ModelForm):
@@ -8,3 +9,20 @@ class ContactRequestForm(forms.ModelForm):
     class Meta:
         model = ContactRequest
         exclude = ('date',)
+
+
+
+class FrameBuzzSignupForm(StripePaymentForm):
+
+    def __init__(self, *args, **kwargs):
+        super(FrameBuzzSignupForm, self).__init__(*args, **kwargs)
+
+    def save(self, request):
+        print 'save'
+        pass
+
+    def signup(self, request, user):
+        print request.raw_post_data
+        print 'signup'
+        super(FrameBuzzSignupForm, self).signup(request, user)
+        pass
