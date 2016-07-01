bf3b9dbc783a6dcd626764f5032e6d56bc249e55
Dani Roxberry <dani@bitoriented.com>
2016-06-30 22:43:37 -0700

    [matches psd] logged out view

--- framebuzz/static/framebuzz/tumblr/js/uploads.js
+++ framebuzz/static/framebuzz/tumblr/js/uploads.js
@@ -1,11 +1,5 @@
 $(function() {
-	$(document).on('click', 'a#btn-login', function(e) {
-		var target = $('#uploader');
-		$.smoothScroll({ scrollElement: null, scrollTarget: target });
-	    return false;
-	});
-
-    $(document).on('click', 'a#tumblr-login-link', function(e) {
+    $(document).on('click', 'a.tumblr-login-link', function(e) {
         var url = $(this).attr('href');
         var newWindow = window.open(url,'frameBuzzSSOLoginWindow','toolbar=0,resizable=0,status=0,width=640,height=528');
         if (window.focus) { newWindow.focus(); }
