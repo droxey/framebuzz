eb30a2e2bf2cfc1abadfbe30f19f3ea2de9acc6d
Dani Roxberry <dani@bitoriented.com>
2017-03-01 05:50:49 -0800

    oembed

--- framebuzz/apps/api/micawber_providers.py
+++ framebuzz/apps/api/micawber_providers.py
@@ -7,6 +7,5 @@ from micawber.providers import Provider
 
 def bootstrap_framebuzz(cached=None, registry=None, **params):
     pr = _bootstrap_basic(cache)
-    #pr.register('https://framebuzz.com/v\/\S*/', Provider('https://framebuzz.com/oembed/', **params))
-    pr.register('http://framebuzz.dev:3333/v\/\S*/', Provider('http://framebuzz.dev:3333/oembed/', **params))
+    pr.register('https://framebuzz.com/v\/\S*/', Provider('https://framebuzz.com/oembed/', **params))
     return pr
