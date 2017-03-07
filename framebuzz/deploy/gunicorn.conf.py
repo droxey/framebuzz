dc3e265b15a0aac4226fd175a18365de5acb6aaa
Dani Roxberry <dani@bitoriented.com>
2017-03-06 19:40:49 -0800

    [fix] clashing ports

--- framebuzz/deploy/gunicorn.conf.py
+++ framebuzz/deploy/gunicorn.conf.py
@@ -1,8 +1,9 @@
 import os
 
-bind = "127.0.0.1:%(gunicorn_port)s"
+bind = "127.0.0.1:9000"
 workers = (os.sysconf("SC_NPROCESSORS_ONLN") * 2) + 1
-proc_name = "%(proj_name)s"
+proc_name = "framebuzz"
+
 
 def when_ready(server):
     from django.core.management import call_command
